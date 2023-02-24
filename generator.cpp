e19b025141e77bd2a4b240d49d74f6d70af373dd
Angelica Engström <angelica.princess@hotmail.com>
2023-02-24 15:24:26 +0100

    returning bools

--- generator.cpp
+++ generator.cpp
@@ -15,15 +15,13 @@ void generator::get_result() {
 }
 
 //<match> := <level>
-std::string match::evaluate(IT it, IT last) {
+bool match::evaluate(IT it, IT last) {
     for(; it < last; it++){
-        std::string s = children[0]->evaluate(it, last);
-        if(!s.empty()){
-            IT start = dynamic_cast<level*>(children[0])->start;
-            for(;it != start; it++){ std::cout << *it; }
+        if(children[0]->evaluate(it, last)){
+            //IT start = dynamic_cast<level*>(children[0])->start;
+            //for(;it != start; it++){ std::cout << *it; }
+            std::cout << "\x1B[32m" << *it << "\033[0m";
 
-            std::cout << "\x1B[32m" + s + "\033[0m";
-            it = it + (int)s.size() - 1;
             continue;
         }
         std::cout << *it;
@@ -44,8 +42,8 @@ void traverse_tree(const std::vector<base*>& children, std::vector<std::vector<b
 }
 
 //<level> := <expressions>\O{<digit>}
-std::string level::evaluate(IT it, IT last) {
-    if(!children[0]->evaluate(it, last).empty()) {
+bool level::evaluate(IT it, IT last) {
+    if(children[0]->evaluate(it, last)) {
         std::vector<std::vector<base*>> nodes;
         nodes.push_back(children);
         traverse_tree(children, nodes);
@@ -55,7 +53,7 @@ std::string level::evaluate(IT it, IT last) {
 
         for(int i = 0; i < index + 1; i++) {
             for (; it != last; it++) {
-                if (!nodes[i][0]->evaluate(it, last).empty()) {
+                if (nodes[i][0]->evaluate(it, last)) {
                     break;
                 }
             }
@@ -67,27 +65,19 @@ std::string level::evaluate(IT it, IT last) {
 }
 
 //<expressions> := <expression> | <expression><expressions>
-std::string expressions::evaluate(IT it, IT last) {
+bool expressions::evaluate(IT it, IT last) {
     if(dynamic_cast<expression*>(children[0])) {
-        std::string result = children[0]->evaluate(it, last);
-        if(!result.empty()){
-            if(ignore){ children[0]->ignore = true; }
-            if(children.size() > 1){
-                std::string next_result = children[1]->evaluate(it + (int)result.size(), last);
-                if(next_result.empty()){
-                    return {};
-                }
-                return result + next_result;
-            }
-            return result;
+        if(ignore){ children[0]->ignore = true; }
+        if(children.size() > 1){
+            return children[1]->evaluate(it, last);
         }
-        return {};
+        return children[0]->evaluate(it, last);
     }
     throw std::runtime_error("Couldn't evaluate expressions");
 }
 
 //<subexpression> := (<expressions>)
-std::string subexpression::evaluate(IT it, IT last) {
+bool subexpression::evaluate(IT it, IT last) {
     if(dynamic_cast<expressions*>(children[0])){
         if(ignore){ children[0]->ignore = true; }
         return children[0]->evaluate(it, last);
@@ -96,16 +86,16 @@ std::string subexpression::evaluate(IT it, IT last) {
 }
 
 //<ignore> := <operand>\I
-std::string ignore::evaluate(IT it, IT last) {
+bool ignore::evaluate(IT it, IT last) {
     for(auto child : children){
         child->ignore = true;
         return child->evaluate(it, last);
     }
-    return {};
+    throw std::runtime_error("Couldn't evaluate ignore");
 }
 
 //<expression> := <operand> | <either> | <many> | <count>
-std::string expression::evaluate(IT it, IT last) {
+bool expression::evaluate(IT it, IT last) {
     for(auto child : children){
         if(ignore){ child->ignore = true; }
         return child->evaluate(it, last);
@@ -114,47 +104,42 @@ std::string expression::evaluate(IT it, IT last) {
 }
 
 //<either> := <operand>+<operand> | <operand>+<either>
-std::string either::evaluate(IT it, IT last){
+bool either::evaluate(IT it, IT last){
     for(auto child : children){
         if(ignore){ child->ignore = true; }
-        if(!child->evaluate(it, last).empty()){
+        if(child->evaluate(it, last)){
             return child->evaluate(it, last);
         }
     }
-    return {};
+    return false;
 }
 
 //<many> := <operand>*
-std::string many::evaluate(IT it, IT last) {
+bool many::evaluate(IT it, IT last) {
     if(ignore){ children[0]->ignore = true; }
     if(it < last) {
-        std::string result = children[0]->evaluate(it, last);
-        if (!result.empty()) {
-            std::string next_result = children[0]->evaluate(it + (int)result.size(), last);
-            if (!next_result.empty()) {
-                return result + evaluate(it + (int)result.size(), last);
+        if (children[0]->evaluate(it, last)) {
+            if (children[0]->evaluate(it + 1, last)) {
+                return evaluate(it + 1, last);
             }
-            return result;
+            return children[0]->evaluate(it + 1, last);
         }
     }
     return {};
 }
 
 //<count> := <operand>{<digit>}
-std::string count::evaluate(IT it, IT last) {
-    std::string eval;
-    std::string result = children[0]->evaluate(it, last);
+bool count::evaluate(IT it, IT last) {
     for(int i = 0; i < *digit - '0'; i++){
-        if (children[0]->evaluate(it + i * (int)result.size(), last).empty()) {
-            return {};
+        if (!children[0]->evaluate(it + i * 1, last)) {
+            return false;
         }
-        eval += children[0]->evaluate(it + i * (int)result.size(), last);
     }
-    return eval;
+    return true;
 }
 
 //<operand> := <string> | <subexpression>
-std::string operand::evaluate(IT it, IT last) {
+bool operand::evaluate(IT it, IT last) {
     if(dynamic_cast<string*>(children[0]) || dynamic_cast<subexpression*>(children[0])){
         if(ignore){ children[0]->ignore = true; }
         return children[0]->evaluate(it, last);
@@ -163,39 +148,31 @@ std::string operand::evaluate(IT it, IT last) {
 }
 
 //<string> := <letter> | <wildcard> | <letter><string> | <wildcard><string>
-std::string string::evaluate(IT it, IT last) {
+bool string::evaluate(IT it, IT last) {
     if(dynamic_cast<wildcard*>(children[0]) || dynamic_cast<letter*>(children[0])) {
         if(ignore){ children[0]->ignore = true; }
-        std::string result = children[0]->evaluate(it, last);
         if (children.size() > 1) {
             if(dynamic_cast<string*>(children[1])) {
                 if(ignore){ children[1]->ignore = true; }
 
-                std::string next_result = children[1]->evaluate(it + 1, it + 2);
-                if (result.empty() || next_result.empty()) {
-                    return {};
+                if (!children[0]->evaluate(it, last) || !children[1]->evaluate(it + 1, it + 2)) {
+                    return false;
                 }
-                return result + next_result;
+                return children[1]->evaluate(it + 1, it + 2);
             }
         }
-        return result;
+        return children[0]->evaluate(it, last);
     }
     throw std::runtime_error("Couldn't evaluate string");
 }
 
-std::string letter::evaluate(IT it, IT last) {
+bool letter::evaluate(IT it, IT last) {
     if(ignore){
-        if(*it == std::tolower(*letter) || *it == std::toupper(*letter)){
-            return std::string{ *it };
-        }
-    }else{
-        if (*it == *letter) {
-            return std::string{ *it };
-        }
+        return *it == std::tolower(*letter) || *it == std::toupper(*letter);
     }
-    return {};
+    return *it == *letter;
 }
 
-std::string wildcard::evaluate(IT it, IT last) {
-    return std::string{ *it };
+bool wildcard::evaluate(IT it, IT last) {
+    return true;
 }
--- generator.h
+++ generator.h
@@ -11,65 +11,65 @@
 using IT = std::string::iterator;
 
 struct base{
-    virtual std::string evaluate(IT it, IT last) = 0;
+    virtual bool evaluate(IT it, IT last) = 0;
     std::vector<base*> children;
     bool ignore = false;
 };
 
 struct match:base{
-    std::string evaluate(IT it, IT last) override;
+    bool evaluate(IT it, IT last) override;
 };
 
 struct level:base{
-    std::string evaluate(IT it, IT last) override;
+    bool evaluate(IT it, IT last) override;
     IT digit;
     IT start;
 };
 
 struct expressions:base{
-    std::string evaluate(IT it, IT last) override;
+    bool evaluate(IT it, IT last) override;
 };
 
 struct subexpression:base{
-    std::string evaluate(IT it, IT last) override;
+    bool evaluate(IT it, IT last) override;
 };
 
 struct expression:base{
-    std::string evaluate(IT it, IT last) override;
+    bool evaluate(IT it, IT last) override;
 };
 
 struct ignore:base{
-    std::string evaluate(IT it, IT last) override;
+    bool evaluate(IT it, IT last) override;
 };
 
 struct either:base{
-    std::string evaluate(IT it, IT last) override;
+    bool evaluate(IT it, IT last) override;
 };
 
 struct count:base{
-    std::string evaluate(IT it, IT last) override;
+    bool evaluate(IT it, IT last) override;
     std::string::iterator digit;
 };
 
 struct many:base{
-    std::string evaluate(IT it, IT last) override;
+    bool evaluate(IT it, IT last) override;
 };
 
 struct operand:base{
-    std::string evaluate(IT it, IT last) override;
+    bool evaluate(IT it, IT last) override;
 };
 
 struct string:base{
-    std::string evaluate(IT it, IT last) override;
+    bool evaluate(IT it, IT last) override;
 };
 
 struct letter:base{
-    std::string evaluate(IT it, IT last) override;
-    std::string::iterator letter;
+    bool evaluate(IT it, IT last) override;
+    IT letter;
 };
 
 struct wildcard:base{
-    std::string evaluate(IT it, IT last) override;
+    bool evaluate(IT it, IT last) override;
 };
 
 class generator {
